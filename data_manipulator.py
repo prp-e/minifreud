83d96d2a98d0d528cb4c162a4dffbb58c78615bc
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-09-07 12:18:53 +0330

    added some prompting notices as well

--- data_manipulator.py
+++ data_manipulator.py
@@ -8,4 +8,10 @@ val_file_name = 'val.txt'
 input_file = open(input_file_name, encoding='utf-8')
 input_content = input_file.read()
 
+split_index = int(0.9 * len(input_content))
+
 vocab = set(input_content)
+train_split = input_content[:split_index]
+val_split = input_content[split_index:]
+
+print(f"All: {len(input_content)}, Train: {len(train_split)}, Val: {len(val_split)}, Vocab Size: {len(vocab)}")
